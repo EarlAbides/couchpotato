d863da2b03e183289ccf3a8259ae99aef428b3f3
Jeffrey Earl <jeffreyearl@hotmail.com>
2016-12-22 10:10:44 -0500

    Use curl instead of wget to download source

--- app.sh
+++ app.sh
@@ -62,7 +62,7 @@ local FOLDER="CouchPotatoServer-build-${VERSION}"
 local FILE="${VERSION}.tar.gz"
 local URL="https://api.github.com/CouchPotato/CouchPotatoServer/tarball/${VERSION}"
 
-_download_tgz "${FILE}" "${URL}" "${FOLDER}"
+_download_tgz_curl "${FILE}" "${URL}" "${FOLDER}"
 mkdir -p "${DEST}/app"
 cp -vfaR "target/${FOLDER}/"* "${DEST}/app/"
 }
